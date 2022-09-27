53809a5e095f7faa33175bb269998a06506eef5b
LearnCodeWithDurgesh <durgeshkumar8896@gmail.com>
2022-09-27 22:41:42 +0530

    project uploaded

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,11 +1,14 @@
 import axios from "axios";
+import { BASE_URL } from "./axios-helper";
 
-const BASE_URL='http://localhost:8081'
+export const createUser = (data) => {
+  return axios
+    .post(`${BASE_URL}/users/`, data)
+    .then((response) => response.data);
+};
 
-export const createUser=(data)=>{
-    return axios.post(`${BASE_URL}/users/`,data).then(response=>response.data)
-}
-
-export const generateToken=(loginData)=>{
-    return axios.post(`${BASE_URL}/auth/login`,loginData).then((response)=>response.data)
-}
+export const generateToken = (loginData) => {
+  return axios
+    .post(`${BASE_URL}/auth/login`, loginData)
+    .then((response) => response.data);
+};
